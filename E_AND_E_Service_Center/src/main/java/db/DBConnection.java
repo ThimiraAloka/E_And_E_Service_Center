f356b0bc25c216d01a9fc8d618f0f6ec661e5f7e
Thimira Aloka M H <150279156+ThimiraAloka@users.noreply.github.com>
2024-01-19 12:26:11 +0530

    add DB connection

--- E_AND_E_Service_Center/src/main/java/db/DBConnection.java
+++ E_AND_E_Service_Center/src/main/java/db/DBConnection.java
@@ -1,10 +1,23 @@
 package db;
 
-import java.net.ConnectException;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_and_e_service_center","root","2001");
+    }
 
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
+        return dbConnection!=null ? dbConnection:(dbConnection=new DBConnection());
+    }
 
-
+    public Connection getConnection(){
+        return connection;
+    }
 }
